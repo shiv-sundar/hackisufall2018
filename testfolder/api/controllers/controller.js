f6a70284ff8318f6d33d0a41800e983d0a6083cc
Ryan Newell <ryansnewell@gmail.com>
2018-10-20 14:31:59 -0500

    Build genesis block

--- testfolder/api/controllers/controller.js
+++ testfolder/api/controllers/controller.js
@@ -31,29 +31,26 @@ function findBarcode(barcode) {
 exports.create_a_block = function(req, res) {
 	var newBlock = new blockSchema(req.body);
 	
-	var relatedBlocks = findBarcode(req.body.barcode);
+	var relatedBlocks = findBarcode(req.body.barcodes);
 
 	var mostRecentTime = new Date(0);
 	var mostRecentBlock;
 
-	if (relatedBlocks.length != 0) {
-	for(block in relatedBlocks){
-		if(block.time_received > mostRecentTime){
-			mostRecentTime = block.time_received;
-			mostRecentBlock = block;
+	if (relatedBlocks == null) {
+		newBlock.hash = "1234567890";
+		newBlock.previous_hash = "0";
+	} else {
+		for(block in relatedBlocks){
+			if(block.time_received > mostRecentTime){
+				mostRecentTime = block.time_received;
+				mostRecentBlock = block;
+			}
 		}
+		newBlock.previous_hash = mostRecentBlock.hash;
+		newBlock.hash = hashBlock(newBlock);
 	}
 
-	newBlock.previous_hash = mostRecentBlock.hash;
-	newBlock.hash = hashBlock(newBlock);
-}
 
-else {
-	newBlock.hash = "31479234";
-	newBlock.previous_hash = "0";
-}
-
-	
 
 	newBlock.save(function(err, task) {
 		if(err)
--- testfolder/api/models/model.js
+++ testfolder/api/models/model.js
@@ -3,11 +3,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var blockSchema = new Schema({
+	block_type: {
+		type: String,
+		default: 'null'
+	},
 	hash: {
 		type: String,
 		default: 'null'
 	},
 	prev_hash: {
+		type: String,
+		default: 'null'
+	},
+	barcodes: {
 		type: String,
 		default: 'null'
 	},
